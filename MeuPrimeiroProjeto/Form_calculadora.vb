ce848b767cd1ac6dca75242388e15195d779c08c
Breh2001 <brendaamaral2001@gmail.com>
2021-08-03 16:30:21 -0300

    Adicinando primeiros eventos aos botões

--- MeuPrimeiroProjeto/Form_calculadora.vb
+++ MeuPrimeiroProjeto/Form_calculadora.vb
@@ -1,3 +1,41 @@
 ﻿Public Class Form_calculadora
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        txtDisplay.Text = "1"
+    End Sub
 
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        txtDisplay.Text = "2"
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        txtDisplay.Text = "3"
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        txtDisplay.Text = "4"
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        txtDisplay.Text = "5"
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        txtDisplay.Text = "6"
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        txtDisplay.Text = "7"
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        txtDisplay.Text = "8"
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        txtDisplay.Text = "9"
+    End Sub
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        txtDisplay.Text = "0"
+    End Sub
 End Class
